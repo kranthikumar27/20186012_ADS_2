4bdc3cbc366cf89a026b916ed7f6dea7f69a4562
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-11-02 10:59:51 +0530

    "ads2_m3_a2_1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignment ADS-2/m3/3.2 Bipartite/Solution.java
+++ Assignment ADS-2/m3/3.2 Bipartite/Solution.java
@@ -2,33 +2,33 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//default constructor.
-	}
-	/**
-	 * main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int numofvertices = Integer.parseInt(scan.nextLine());
-		int numofedges = Integer.parseInt(scan.nextLine());
-		Graph objgraph = new Graph(numofvertices);
-		while (scan.hasNext()) {
-			String[] input = scan.nextLine().split(" ");
-			objgraph.addEdge(Integer.parseInt(input[0]),
-			                 Integer.parseInt(input[1]));
-		}
-		DirectedCycle objdc = new DirectedCycle(objgraph);
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //default constructor.
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numofvertices = Integer.parseInt(scan.nextLine());
+        int numofedges = Integer.parseInt(scan.nextLine());
+        Graph objgraph = new Graph(numofvertices);
+        while (scan.hasNext()) {
+            String[] input = scan.nextLine().split(" ");
+            objgraph.addEdge(Integer.parseInt(input[0]),
+                             Integer.parseInt(input[1]));
+        }
+        DirectedCycle objdc = new DirectedCycle(objgraph);
         if (objdc.isBipartite()) {
             System.out.println("Graph is bipartite");
         } else {
             System.out.println("Graph is not a bipartite");
         }
-	}
+    }
 }
