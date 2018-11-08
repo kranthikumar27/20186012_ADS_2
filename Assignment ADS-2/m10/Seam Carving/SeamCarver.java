4253d2cdfbb20534f96f02845363cd259a86bc7f
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-11-08 16:09:32 +0530

    "ads2_m10_a1_1: 3/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignment ADS-2/m10/Seam Carving/SeamCarver.java
+++ Assignment ADS-2/m10/Seam Carving/SeamCarver.java
@@ -1,4 +1,4 @@
-
+import java.awt.Color;
 public class SeamCarver {
 	private Picture picture;
 	// create a seam carver object based on the given picture
@@ -25,7 +25,23 @@ public class SeamCarver {
 
 	// energy of pixel at column x and row y
 	public double energy(int x, int y) {
-		return 0;
+		if (x == 0 || y == 0 || picture.width() - 1 == x || picture.height() - 1 == y) {
+			return 1000;
+		}
+		Color top = picture.get(x, y + 1);
+		Color bottom = picture.get(x, y - 1);
+		Color left = picture.get(x - 1, y);
+		Color right = picture.get(x + 1, y);
+		int rh = Math.abs(right.getRed() - left.getRed());
+		int gh = Math.abs(right.getGreen() - left.getGreen());
+		int bh = Math.abs(right.getBlue() - left.getBlue());
+		int horizontal = rh * rh + gh * gh + bh * bh;
+		int rv = Math.abs(top.getRed() - bottom.getRed());
+		int gv = Math.abs(top.getGreen() - bottom.getGreen());
+		int bv = Math.abs(top.getBlue() - bottom.getBlue());
+		int vertical = rv * rv + gv * gv + bv * bv;
+		double energy = horizontal + vertical;
+		return energy;
 	}
 
 	// sequence of indices for horizontal seam
