7c3b3f1ce86b99bf471fd63f6d859d0caa852ce9
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-11-14 17:38:00 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignment ADS-2/m14/Assignment-1/Solution.java
+++ Assignment ADS-2/m14/Assignment-1/Solution.java
@@ -1,46 +1,45 @@
 import java.util.Scanner;
-/**
+/**.
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * default constructor.
-	 */
-	private Solution() {
-		//default constructor.
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		String[] words = loadWords();
-		//Your code goes here...
-		TST<Integer> tst = new TST<Integer>();
-		Scanner scan = new Scanner(System.in);
-		String prefix = scan.nextLine();
-		int j = 0;
-		for (String each : words) {
-			SuffixArray suffixobj = new SuffixArray(each);
-			for (int i = 0; i < each.length() ; i++) {
-				tst.put(suffixobj.select(i), j++);
-			}
-		}
-		for (String each : tst.keysWithPrefix(prefix)) {
-			System.out.println(each);
-		}
-	}
-	/**
-	 * Loads words.
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public static String[] loadWords() {
-		In in = new In("/Files/dictionary-algs4.txt");
-		String[] words = in.readAllStrings();
-		return words;
-	}
-
-
+    /**.
+     * default constructor.
+     */
+    private Solution() {
+        //default constructor.
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        String[] words = loadWords();
+        //Your code goes here...
+        TST<Integer> tst = new TST<Integer>();
+        Scanner scan = new Scanner(System.in);
+        String prefix = scan.nextLine();
+        int j = 0;
+        for (String each : words) {
+            SuffixArray suffixobj = new SuffixArray(each);
+            for (int i = 0; i < each.length(); i++) {
+                tst.put(suffixobj.select(i), j++);
+            }
+        }
+        for (String each : tst.keysWithPrefix(prefix)) {
+            System.out.println(each);
+        }
+    }
+    /**.
+     * Loads words.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static String[] loadWords() {
+        In in = new In("/Files/dictionary-algs4.txt");
+        String[] words = in.readAllStrings();
+        return words;
+    }
 }
+
